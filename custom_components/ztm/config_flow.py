bad9edec8a61211de6ee32de49b86a103dab574a
peetereczek <42431204+peetereczek@users.noreply.github.com>
2024-03-15 17:58:40 +0100

    Update config_flow.py

--- custom_components/ztm/config_flow.py
+++ custom_components/ztm/config_flow.py
@@ -3,6 +3,7 @@ import time
 
 from homeassistant                  import config_entries, data_entry_flow
 from homeassistant.config_entries   import ConfigEntry as config_entry
+from .const                         import DOMAIN
 from homeassistant.data_entry_flow  import FlowHandler, FlowResult
 from homeassistant.core             import callback, HomeAssistant
 from homeassistant.util             import slugify
@@ -12,3 +13,14 @@ from homeassistant.helpers          import (selector, entity_registry as er, dev
 import homeassistant.helpers.config_validation as cv
 import homeassistant.util.dt as dt_util
 import voluptuous as vol
+
+import logging
+
+_LOGGER = logging.getLogger("ztm")
+
+class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
+    # The schema version of the entries that it creates
+    # Home Assistant will call your migrate method if the version changes
+    VERSION = 0
+    MINOR_VERSION = 1
