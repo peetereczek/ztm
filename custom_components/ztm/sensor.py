ce5f78b43a4a55f6ba3a69174c1167e0e96d1408
peetereczek <42431204+peetereczek@users.noreply.github.com>
2024-03-16 19:58:03 +0100

    Update sensor.py

--- custom_components/ztm/sensor.py
+++ custom_components/ztm/sensor.py
@@ -12,64 +12,35 @@ import async_timeout
 import voluptuous as vol
 import re
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME, CONF_API_KEY)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import Entity
 import homeassistant.util.dt as dt_util
+from .const import (DOMAIN,
+                    DEFAULT_NAME,
+                    CONF_ATTRIBUTION,
+                    ZTM_ENDPOINT,
+                    ZTM_DATA_ID,
+                    CONF_API_KEY,
+                    REQUEST_TIMEOUT,
+                    UNIT,
+                    SENSOR_ID_FORMAT,
+                    SENSOR_NAME_FORMAT,
+                    CONF_LINES,
+                    CONF_LINE_NUMBER,
+                    CONF_STOP_ID,
+                    CONF_STOP_NUMBER,
+                    CONF_ENTRIES,
+                    DEFAULT_ENTRIES)
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_ATTRIBUTION = ("Dane dostarcza Miasto Stołeczne Warszawa, api.um.warszawa.pl")
-ZTM_ENDPOINT = "https://api.um.warszawa.pl/api/action/dbtimetable_get/"
-ZTM_DATA_ID = 'e923fa0e-d96c-43f9-ae6e-60518c9f3238'
-
-REQUEST_TIMEOUT = 5  # seconds
 SCAN_INTERVAL = timedelta(minutes=1)
-UNIT = 'min'
-
-DEFAULT_NAME = "ZTM"
-SENSOR_ID_FORMAT = "{} {} from {} {}"
-SENSOR_NAME_FORMAT = "{} {} from {} {}"
-
-CONF_LINES = 'lines'
-CONF_LINE_NUMBER = 'number'
-CONF_STOP_ID = 'stop_id'
-CONF_STOP_NUMBER = 'stop_number'
-CONF_ENTRIES = 'entries'
-DEFAULT_ENTRIES = 3
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_LINES): vol.All(cv.ensure_list, [{
-        vol.Required(CONF_LINE_NUMBER): cv.string,
-        vol.Required(CONF_STOP_ID): cv.string,
-        vol.Required(CONF_STOP_NUMBER): cv.string}]),
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_ENTRIES, default=DEFAULT_ENTRIES): cv.positive_int,
-    })
-
-async def async_setup_platform(hass, config, async_add_devices,
-                               discovery_info=None):
-    """Set up the ZTM platform."""
-    websession = async_get_clientsession(hass)
-    api_key = config[CONF_API_KEY]
-    prepend = config[CONF_NAME]
-    entries = config[CONF_ENTRIES]
-    lines = []
-    for line_config in config.get(CONF_LINES):
-        line = line_config[CONF_LINE_NUMBER]
-        stop_id = line_config[CONF_STOP_ID]
-        stop_number = line_config[CONF_STOP_NUMBER]
-        identifier = SENSOR_ID_FORMAT.format(prepend, line, stop_id, stop_number)
-        lines.append(ZTMSensor(hass.loop, websession, api_key, line, stop_id,
-                               stop_number, identifier, entries))
-    async_add_devices(lines)
-
 
 class ZTMSensor(Entity):
     """Implementation of a ZTM sensor."""
+    _attr_has_entity_name = True
 
     def __init__(self, loop, websession, api_key, line, stop_id, stop_number,
                  identifier, entries):
@@ -79,7 +50,7 @@ class ZTMSensor(Entity):
         self._line = line
         self._stop_id = stop_id
         self._stop_number = stop_number
-        self._name = identifier
+        self._name = SENSOR_NAME_FORMAT.format("ZTM", line, stop_id, stop_number)
         self._entries = entries
         self._state = None
         self._icon = ''
@@ -152,7 +123,7 @@ class ZTMSensor(Entity):
             else:
                 self._timetable = self.map_results(res.get('response', [0]))
                 self._timetable_date = dt_util.now().date()
-                _LOGGER.debug("Downloaded timetable for line: %s stop: %s-%s",
+                _LOGGER.debug("wnloaded timetable for line: %s stop: %s-%s",
                               self._line, self._stop_id, self._stop_number)
                 _LOGGER.debug("TIMETABLE: %s", self._timetable)
         # check if there are trains after actual time
